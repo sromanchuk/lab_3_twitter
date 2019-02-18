5fb26437fb52d64da74eb8e02556e30252a67f9a
sromanchuk <47135579+sromanchuk@users.noreply.github.com>
2019-02-18 23:28:32 +0200

    Update twitter_api.py

--- twitter_api.py
+++ twitter_api.py
@@ -4,25 +4,33 @@ import json
 import ssl
 
 
-def data(json, type):
+def data_search(json, type):
+
     lst = []
-    for user in json["user"]:
-        lst.append(user[type])
+    for user in json["users"]:
+        lst.append([user["name"], user[type]])
     return lst
 
-if __name__ == "__main__":
+
+def data_name(name):
+    
     TWITTER_URL = 'https://api.twitter.com/1.1/friends/list.json'
     ctx = ssl.create_default_context()
     ctx.check_hostname = False
     ctx.verify_mode = ssl.CERT_NONE
-    print('')
-    acct = input('Enter Twitter Account:')
-    # (len(acct) < 1): break
     url = twurl.augment(TWITTER_URL,
-                        {'screen_name': acct, 'count': '200'})
+                        {'screen_name': name, 'count': '200'})
     print('Retrieving', url)
     connection = urllib.request.urlopen(url, context=ctx)
     data = connection.read().decode()
+
     js = json.loads(data)
-    type = str(input("Type the type: "))
-    print(data(js, type))
+    return js
+
+
+if __name__ == "__main__":
+    print('')
+    acct = input('Enter Twitter Account:')
+    type = input("Type that you want to see from the users info : ")
+    js = data_name(acct)
+    print(data_search(js, type))
